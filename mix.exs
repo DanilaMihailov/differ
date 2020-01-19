ab1e696e0e84e9aea59b2d2a7aea664ee3d34dc0
Danila Mihailov <danilamihailov@icloud.com>
2020-01-19 17:26:01 +0500

    added test coverage package

--- mix.exs
+++ mix.exs
@@ -8,7 +8,9 @@ defmodule Differ.MixProject do
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      docs: docs()
+      docs: docs(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test]
     ]
   end
 
@@ -24,7 +26,8 @@ defmodule Differ.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.12.1", only: :test}
     ]
   end
 
