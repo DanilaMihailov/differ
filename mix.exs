1b5b3e60ab0471a75f28da9be534aed9ec02bfec
Danila Mihailov <danilamihailov@icloud.com>
2020-01-19 17:46:26 +0500

    fixed formatting

--- mix.exs
+++ mix.exs
@@ -10,7 +10,12 @@ defmodule Differ.MixProject do
       deps: deps(),
       docs: docs(),
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
