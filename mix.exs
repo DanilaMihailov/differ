70d920b342b9f057dc542d203f4baf90a232395d
Danila Mihailov <danilamihailov@icloud.com>
2020-01-19 17:33:33 +0500

    updated coverage params

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule Differ.MixProject do
       deps: deps(),
       docs: docs(),
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [coveralls: :test]
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
     ]
   end
 
