89fc29d591f5bfbf89af97d2050c70efc2979a24
Danila Mihailov <danilamihailov@icloud.com>
2020-01-19 18:50:11 +0500

    added source url and homepage

--- mix.exs
+++ mix.exs
@@ -5,6 +5,8 @@ defmodule Differ.MixProject do
     [
       app: :differ,
       version: "0.1.0",
+      source_url: "https://github.com/DanilaMihailov/Differ",
+      homepage_url: "https://github.com/DanilaMihailov/Differ",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
