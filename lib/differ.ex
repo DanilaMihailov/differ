ee372c5a85e3fc5a0a882a9e5b7e7c86403adfb8
Danila Mihailov <danilamihailov@icloud.com>
2020-01-20 18:39:17 +0500

    moved types to diffable module

--- lib/differ.ex
+++ lib/differ.ex
@@ -1,39 +1,15 @@
 defmodule Differ do
+  alias Differ.Diffable
+  alias Differ.Patchable
+
   @moduledoc "Module that computes diff for objects"
-
-  @typedoc """
-  Operators that define how to change data
-
-  * `:del` - delete
-  * `:ins` - insert
-  * `:eq` - doesnt change
-  * `:diff` - nested diff that should be applied
-  * `:skip` - skip number of characters or elements
-  * `:remove` - remove number of characters or elements (Non-revertable)
-  """
-  @type operator() :: :del | :ins | :eq | :diff | :skip | :remove
-
-  @typedoc """
-  Defines operator and value that need to be applied with operator
-  ## Examples
-      {:del, "s"}
-      {:skip, 4}
-      {"key", :ins, "s"}
-  """
-  @type operation() :: {operator(), any} | {any, operator(), any}
-
-  @typedoc "Types that supported by `Differ.diff/2`"
-  @type diffable() :: String.t() | number() | %{any() => diffable()} | list(diffable())
-
-  @typedoc "List of operations need to be applied"
-  @type diff() :: list(operation())
 
   @doc """
   Returns diff between 2 objects of same type
   """
-  @spec diff(diffable(), diffable()) :: diff()
+  @spec diff(Diffable.t(), Diffable.t()) :: Diffable.Diffable.diff()
   def diff(old, new) do
-    Differ.Diffable.diff(old, new)
+    Diffable.diff(old, new)
   end
 
   @doc """
@@ -45,7 +21,7 @@ defmodule Differ do
       iex(2)> Differ.patch old_list, diff
       {:ok, ["2", "1", "3"]}
   """
-  @spec patch(diffable(), diff()) :: {:ok, diffable()} | {:error, String.t()}
+  @spec patch(Diffable.t(), Diffable.diff()) :: {:ok, Diffable.t()} | {:error, String.t()}
   def patch(obj, diff) do
     apply_diff(obj, diff, false)
   end
@@ -60,7 +36,7 @@ defmodule Differ do
       iex(2)> Differ.revert new_list, diff
       {:ok, ["22", "1"]}
   """
-  @spec revert(diffable(), diff()) :: {:ok, diffable()} | {:error, String.t()}
+  @spec revert(Diffable.t(), Diffable.diff()) :: {:ok, Diffable.t()} | {:error, String.t()}
   def revert(obj, diff) do
     apply_diff(obj, diff, true)
   end
@@ -79,7 +55,7 @@ defmodule Differ do
       iex> Differ.optimize(diff)
       [skip: 41, del: "t", skip: 4]
   """
-  @spec optimize(diff(), boolean()) :: diff()
+  @spec optimize(Diffable.diff(), boolean()) :: Diffable.diff()
   def optimize(diff, revertable \\ true) do
     Enum.reduce(diff, [], fn change, acc ->
       case change do
@@ -124,7 +100,7 @@ defmodule Differ do
         case diff_res do
           {:ok, val} ->
             new_op = Tuple.append(op, val)
-            Differ.Patchable.perform(old_val, new_op, acc) |> match_res(old_val, acc, revert)
+            Patchable.perform(old_val, new_op, acc) |> match_res(old_val, acc, revert)
 
           _ ->
             {:halt, res}
@@ -138,8 +114,8 @@ defmodule Differ do
   defp apply_diff(old_val, diff, revert) do
     result =
       Enum.reduce_while(diff, {old_val, 0}, fn op, acc ->
-        op = if revert, do: Differ.Patchable.revert_op(old_val, op), else: op
-        Differ.Patchable.perform(old_val, op, acc) |> match_res(old_val, acc, revert)
+        op = if revert, do: Patchable.revert_op(old_val, op), else: op
+        Patchable.perform(old_val, op, acc) |> match_res(old_val, acc, revert)
       end)
 
     case result do
--- lib/protocols/diffable.ex
+++ lib/protocols/diffable.ex
@@ -1,4 +1,34 @@
 defprotocol Differ.Diffable do
+  alias Differ.Diffable
   @fallback_to_any true
+  @moduledoc """
+  Allows to compute difference between objects
+  """
+
+  @typedoc """
+  Operators that define how to change data
+
+  * `:del` - delete
+  * `:ins` - insert
+  * `:eq` - doesnt change
+  * `:diff` - nested diff that should be applied
+  * `:skip` - skip number of characters or elements
+  * `:remove` - remove number of characters or elements (Non-revertable)
+  """
+  @type operator() :: :del | :ins | :eq | :diff | :skip | :remove
+
+  @typedoc """
+  Defines operator and value that need to be applied with operator
+  ## Examples
+      {:del, "s"}
+      {:skip, 4}
+      {"key", :ins, "s"}
+  """
+  @type operation() :: {operator(), term} | {term, operator(), term}
+
+  @typedoc "List of operations need to be applied"
+  @type diff() :: list(operation())
+
+  @spec diff(Diffable.t(), Diffable.t()) :: diff
   def diff(term1, term2)
 end
