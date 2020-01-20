c70aefe03676e6394aca8a3471285e20cfb2e6ca
Danila Mihailov <danilamihailov@icloud.com>
2020-01-20 16:12:36 +0500

    moved string tests to string module

--- test/differ_test.exs
+++ test/differ_test.exs
@@ -59,47 +59,4 @@ defmodule DifferTest do
 
     assert expected_output == output
   end
-
-  test "string diffs are patchable" do
-    old_str = "My name is Dan"
-    new_str = "Hello, I'am Danila"
-
-    diff = Differ.diff(old_str, new_str)
-    op_diff = Differ.optimize(diff)
-    non_rev_diff = Differ.optimize(diff, false)
-    invalid_diff = [{:some_random_operation, "do it"} | diff]
-
-    assert {:ok, new_str} == Differ.patch(old_str, diff)
-    assert {:ok, new_str} == Differ.patch(old_str, op_diff)
-    assert {:ok, new_str} == Differ.patch(old_str, non_rev_diff)
-
-    assert {:error, "Unknown operation {some_random_operation, do it} for diff of type string"} ==
-             Differ.patch(old_str, invalid_diff)
-  end
-
-  test "string diffs are revertable" do
-    old_str = "My name is Dan"
-    new_str = "Hello, I'am Danila"
-
-    diff = Differ.diff(old_str, new_str)
-    op_diff = Differ.optimize(diff)
-    non_rev_diff = Differ.optimize(diff, false)
-    invalid_diff = [{:some_random_operation, "do it"} | diff]
-
-    assert {:ok, old_str} == Differ.revert(new_str, diff)
-    assert {:ok, old_str} == Differ.revert(new_str, op_diff)
-    assert {:error, "This diff is not revertable"} == Differ.revert(new_str, non_rev_diff)
-
-    assert {:error, "Unknown operation {some_random_operation, do it} for diff of type string"} ==
-             Differ.revert(old_str, invalid_diff)
-  end
-
-  test "string diff without deletion are revertable" do
-    old_str = "Hello, "
-    new_str = "Hello, World!"
-
-    diff = Differ.diff(old_str, new_str) |> Differ.optimize(false)
-
-    assert {:ok, old_str} == Differ.revert(new_str, diff)
-  end
 end
