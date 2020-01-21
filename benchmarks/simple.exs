39a074268276b6ef450879b3cecabf0915bbaac3
Danila Mihailov <danilamihailov@icloud.com>
2020-01-21 12:57:06 +0500

    changed benchmark to allow comparison

--- benchmarks/simple.exs
+++ benchmarks/simple.exs
@@ -2,15 +2,16 @@ old_user = %{name: "Ron Artest", teams: ["Bulls", "Pacers"], height: 201}
 new_user = %{name: "Metta World Peace", teams: ["Bulls", "Pacers", "Kings", "Rockets"], height: 201, weight: 118}
 
 diff = Differ.diff(old_user, new_user)
+l0 = List.duplicate(diff, 10)
+l1 = diff |> Differ.optimize(1) |> List.duplicate(10)
+l2 = diff |> Differ.optimize(2) |> List.duplicate(10)
+l3 = diff |> Differ.optimize(3) |> List.duplicate(10)
 
 Benchee.run(
   %{
-    "Reasonable diff" => fn diffs -> Enum.map(diffs, &(Differ.patch(old_user, &1))) end
-  },
-  inputs: %{
-    "Level 0" => List.duplicate(diff, 10),
-    "Level 1" => diff |> Differ.optimize(1) |> List.duplicate(10),
-    "Level 2" => diff |> Differ.optimize(2) |> List.duplicate(10),
-    "Level 3" => diff |> Differ.optimize(3) |> List.duplicate(10),
+    "Patch level 0" => fn -> Enum.map(l0, &(Differ.patch(old_user, &1))) end,
+    "Patch level 1" => fn -> Enum.map(l1, &(Differ.patch(old_user, &1))) end,
+    "Patch level 2" => fn -> Enum.map(l2, &(Differ.patch(old_user, &1))) end,
+    "Patch level 3" => fn -> Enum.map(l3, &(Differ.patch(old_user, &1))) end
   }
 )
