f5a27f30942f0e1368235c5c82ec78aeb3fbce4c
Danila Mihailov <danilamihailov@icloud.com>
2020-01-21 12:16:12 +0500

    added optimization test

--- benchmarks/test.exs
+++ benchmarks/test.exs
@@ -1,12 +1,16 @@
-old_user = %{name: "Ron Artest", teams: ["Bulls", "Pacers"]}
-new_user = %{name: "Metta World Peace", teams: ["Bulls", "Pacers", "Kings", "Rockets"]}
-diff = Differ.diff(old_user, new_user)
+old_user = %{name: "Ron Artest", teams: ["Bulls", "Pacers"], height: 201}
+new_user = %{name: "Metta World Peace", teams: ["Bulls", "Pacers", "Kings", "Rockets"], height: 201, weight: 118}
 
-list = List.duplicate(diff, 10)
-map_fun = fn d -> Differ.patch(old_user, diff) end
+diff = Differ.diff(old_user, new_user)
 
 Benchee.run(
   %{
-    "Reasonable diff" => fn -> Enum.map(list, map_fun) end
+    "Reasonable diff" => fn diffs -> Enum.map(diffs, &(Differ.patch(old_user, &1))) end
+  },
+  inputs: %{
+    "Level 0" => List.duplicate(diff, 10),
+    "Level 1" => diff |> Differ.optimize(1) |> List.duplicate(10),
+    "Level 2" => diff |> Differ.optimize(2) |> List.duplicate(10),
+    "Level 3" => diff |> Differ.optimize(3) |> List.duplicate(10),
   }
 )
